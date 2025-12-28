51329698391dd809c35e71f7977e2d7bfc53e53a
6xszrxr4rr-art <6xszrxr4rr@privaterelay.appleid.com>
2025-12-28 10:27:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -80,7 +80,6 @@ function ensureAudioGraph() {
   audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
   const bg = $("bgMusic");
-ensureAudioGraph();
 if (audioCtx && audioCtx.state === "suspended") audioCtx.resume();
   if (!bg) return;
 
