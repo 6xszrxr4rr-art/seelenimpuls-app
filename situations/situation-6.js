d53c688743bb26fba3213ffb6fcfb3986061506b
6xszrxr4rr-art <6xszrxr4rr@privaterelay.appleid.com>
2026-02-06 21:15:54 +0100

    Update situation-6.js

--- situations/situation-6.js
+++ situations/situation-6.js
@@ -1,6 +1,7 @@
 window.SITUATIONS = window.SITUATIONS || {};
 
 window.SITUATIONS[6] = {
+  title: "Innere Leere & Orientierungslosigkeit",
   id: 6,
   title: "6) Innere Leere & Orientierungslosigkeit",
   hint: "Tippe hier, wenn innen wenig greifbar ist.",
